e4aa0fd8517381c34f7ffe9b1d927620eb484793
hopbest <minhtuan2898@gmail.com>
2019-08-03 21:35:20 +0700

    Finish user sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,25 @@
 class UsersController < ApplicationController
   def new
+  	@user = User.new
+  end
+
+  def show
+  	@user = User.find(params[:id])
+  end
+
+  def create
+  	@user = User.new(users_params)
+  	if @user.save
+      flash[:success] = "Welcome to the Full App!"
+      redirect_to user_url(@user)
+  	else 
+  		render 'new'
+  	end
+  end
+
+  private
+
+  def users_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
